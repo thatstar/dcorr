01785f322c7b862e84ebf849dd371114c6697141
Su Rui <surui1986@gmail.com>
2020-01-07 09:37:38 +0800

    Param bounds.

--- dcorr/kww.py
+++ dcorr/kww.py
@@ -2,11 +2,13 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 
-def kww(t, ta, alpha, beta):
-    return alpha*np.exp(-(t/ta)**beta)
+def kww(t, talpha, A, beta):
+    return A*np.exp(-(t/talpha)**beta)
 
 
 def kww_fit(xdata, ydata):
-    popt, _ = curve_fit(kww, xdata, ydata)
+    param_bounds = ([np.finfo(float).eps, -np.inf, -np.inf],
+                    [np.inf, np.inf, np.inf])
+    popt, _ = curve_fit(kww, xdata, ydata, bounds=param_bounds)
 
     return popt
